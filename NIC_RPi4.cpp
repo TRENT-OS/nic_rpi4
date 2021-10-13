f3fb75d523b3d404aa97c1a783599b27b3529538
Lukas Graber <lukas.graber@hensoldt-cyber.de>
2021-10-13 11:54:58 +0200

    [DEV] SEOS-2345: Implement RPi4 NIC driver

---
+++ NIC_RPi4.cpp
@@ -0,0 +1,241 @@
+/*
+ * Copyright (C) 2021, HENSOLDT Cyber GmbH
+ */
+
+extern "C"
+{
+#include "OS_Error.h"
+#include "OS_Dataport.h"
+#include "lib_debug/Debug.h"
+#include "network/OS_NetworkStackTypes.h"
+#include <sel4/sel4.h>
+#include <circleos.h>
+#include <camkes.h>
+}
+
+#include <string.h>
+#include <stdlib.h>
+#include <circle/bcm54213.h>
+#include "environment.h"
+
+void*
+operator new(size_t n)
+{
+    void* const p = dma_alloc(DMA_PAGE_SIZE, DMA_ALIGNEMENT);
+
+    return p;
+}
+
+void*
+operator new[](size_t n)
+{
+    void* const p = dma_alloc(DMA_PAGE_SIZE, DMA_ALIGNEMENT);
+
+    return p;
+}
+
+void
+operator delete(
+    void* p,
+    size_t sz)
+{
+    dma_free(p, DMA_ALIGNEMENT);
+}
+
+void
+operator delete[](void* p)
+{
+    dma_free(p, DMA_ALIGNEMENT);
+}
+
+static CBcm54213Device* bcm54213Device;
+static volatile boolean initOk = false;
+
+extern "C"
+{
+
+void
+post_init(void)
+{
+    ps_io_ops_t io_ops;
+    int ret = camkes_io_ops(&io_ops);
+    if (0 != ret)
+    {
+        Debug_LOG_ERROR("camkes_io_ops() failed - error code: %d", ret);
+        return;
+    }
+
+    // mailbox initialization
+    ret = mbox_init(&io_ops);
+    if (0 != ret)
+    {
+        Debug_LOG_ERROR("Mailbox initialization failed!");
+        return;
+    }
+
+    bcm54213Device = new CBcm54213Device();
+    initOk = true;
+}
+
+int
+run(void)
+{
+    if (!initOk)
+    {
+        Debug_LOG_ERROR("NIC driver not (correctly) intitialized");
+        return OS_ERROR_NOT_INITIALIZED;
+    }
+
+    if (!bcm54213Device->Initialize())
+    {
+        Debug_LOG_ERROR("Cannot initialize BCM54213");
+        return OS_ERROR_GENERIC;
+    }
+
+    Debug_LOG_INFO("[EthDrv '%s'] starting", get_instance_name());
+
+    unsigned timeout = 0;
+    while (!bcm54213Device->IsLinkUp())
+    {
+        MsDelay (100);
+
+        if (++timeout < 40)
+        {
+            continue;
+        }
+        timeout = 0;
+
+        Debug_LOG_WARNING("Link is down");
+    }
+
+    Debug_LOG_DEBUG("Link is up");
+
+    if (!nic_driver_init_done_post())
+    {
+        Debug_LOG_DEBUG("Post failed.");
+    }
+
+    uint8_t* Buffer = (uint8_t*)dma_alloc(DMA_PAGE_SIZE, DMA_ALIGNEMENT);
+    size_t receivedLength = 0;
+
+    unsigned int count = 0;
+    while (true)
+    {
+        if (bcm54213Device->ReceiveFrame(Buffer, (u32*)&receivedLength))
+        {
+            OS_NetworkStack_RxBuffer_t* buf_ptr = (OS_NetworkStack_RxBuffer_t*)nic_to_port;
+
+            if (receivedLength > sizeof(buf_ptr->data) )
+            {
+                Debug_LOG_WARNING(
+                    "The max length of the data is %u, but the length of the "
+                    "read data is %ld",
+                    (unsigned)sizeof(buf_ptr->data),
+                    receivedLength);
+                // throw away current frame and read the next one
+                continue;
+            }
+
+            // if the slot to be used in the ringbuffer isn't empty we
+            // wait here in a loop
+            // TODO: Implement it in an event driven fashion
+            while (buf_ptr[count].len != 0)
+            {
+                seL4_Yield();
+            }
+            memcpy(buf_ptr[count].data, Buffer, receivedLength);
+            buf_ptr[count].len = receivedLength;
+            count = (count + 1) % NIC_DRIVER_RINGBUFFER_NUMBER_ELEMENTS;
+            nic_event_hasData_emit();
+        }
+    }
+
+    dma_free(Buffer, DMA_ALIGNEMENT);
+
+    return OS_SUCCESS;
+}
+
+
+/* nic_rpc interface ----------------------------------------------------------------*/
+
+OS_Error_t
+nic_rpc_rx_data(
+    size_t* pLen,
+    size_t* framesRemaining)
+{
+    if (!initOk)
+    {
+        Debug_LOG_ERROR("NIC driver not (correctly) intitialized");
+        return OS_ERROR_NOT_INITIALIZED;
+    }
+
+    return OS_ERROR_NOT_IMPLEMENTED;
+}
+
+OS_Error_t
+nic_rpc_tx_data(
+    size_t* len)
+{
+    if (!initOk)
+    {
+        Debug_LOG_ERROR("NIC driver not (correctly) intitialized");
+        return OS_ERROR_NOT_INITIALIZED;
+    }
+
+    if (!bcm54213Device->SendFrame(nic_from_port, *len))
+    {
+        Debug_LOG_ERROR("Bcm54213SendFrame failed");
+        return OS_ERROR_ABORTED;
+    }
+
+    return OS_SUCCESS;
+}
+
+OS_Error_t
+nic_rpc_get_mac_address(void)
+{
+    if (!initOk)
+    {
+        Debug_LOG_ERROR("NIC driver not (correctly) intitialized");
+        return OS_ERROR_NOT_INITIALIZED;
+    }
+
+    if (!nic_driver_init_done_wait())
+    {
+        Debug_LOG_DEBUG("Wait failed.");
+    }
+
+    GetMACAddress((uint8_t*)nic_to_port);
+
+    return OS_SUCCESS;
+}
+
+/* Genet interrupt handler A-----------------------------------------------------------------*/
+void
+genetA_BaseIrq_handle(void)
+{
+    bcm54213Device->InterruptHandler0();
+
+    int error = genetA_BaseIrq_acknowledge();
+
+    if (error != 0)
+    {
+        Debug_LOG_ERROR("Failed to acknowledge the interrupt!");
+    }
+}
+
+/* Genet interrupt handler B-----------------------------------------------------------------*/
+void
+genetB_BaseIrq_handle(void)
+{
+    bcm54213Device->InterruptHandler1();
+
+    int error = genetB_BaseIrq_acknowledge();
+
+    if (error != 0)
+    {
+        Debug_LOG_ERROR("Failed to acknowledge the interrupt!");
+    }
+}
+
+}
---
+++ environment.cpp
@@ -0,0 +1,206 @@
+/*
+ * Copyright (C) 2021, HENSOLDT Cyber GmbH
+ */
+
+extern "C"
+{
+#include <stdio.h>
+#include <string.h>
+#include <stdarg.h>
+
+#include <circleos.h>
+
+#include "lib_debug/Debug.h"
+
+#include "environment.h"
+
+#include "TimeServer.h"
+
+#include <camkes.h>
+#include <camkes/dma.h>
+#include <camkes/io.h>
+
+static mailbox_t mbox;
+
+static const if_OS_Timer_t timer =
+    IF_OS_TIMER_ASSIGN(
+        timeServer_rpc,
+        timeServer_notify);
+
+/* Environment functions -----------------------------------------------------*/
+
+void*
+dma_alloc(
+    unsigned nSize,
+    unsigned alignement)
+{
+    // we are setting cached to false to allocate non-cached DMA memory for the
+    // NIC driver
+    return camkes_dma_alloc(nSize, alignement, false);
+}
+
+void
+dma_free(
+    void* pBlock,
+    unsigned alignement)
+{
+    camkes_dma_free(pBlock, alignement);
+}
+
+uintptr_t
+dma_getPhysicalAddr(void* ptr)
+{
+    return camkes_dma_get_paddr(ptr);
+}
+
+void
+LogWrite(
+    const char* pSource,
+    unsigned Severity,
+    const char* pMessage,
+    ...)
+{
+    va_list args;
+    va_start (args, pMessage);
+
+    switch (Severity)
+    {
+    case CIRCLE_LOG_ERROR:
+        printf("\n ERROR %s: - ", pSource);
+        break;
+    case CIRCLE_LOG_WARNING:
+        printf("\n WARNING %s: - ", pSource);
+        break;
+    case CIRCLE_LOG_NOTICE:
+        printf("\n NOTICE %s: - ", pSource);
+        break;
+    case CIRCLE_LOG_DEBUG:
+        printf("\n DEBUG %s: - ", pSource);
+        break;
+    }
+
+    vprintf(pMessage, args);
+    printf("\n");
+    va_end(args);
+}
+
+void
+MsDelay(unsigned nMilliSeconds)
+{
+    usDelay(1000 * nMilliSeconds);
+}
+
+void
+usDelay(unsigned nMicroSeconds)
+{
+    nsDelay(1000 * nMicroSeconds);
+}
+
+void
+nsDelay(unsigned nNanoSeconds)
+{
+    OS_Error_t err;
+
+    if ((err = TimeServer_sleep(&timer, TimeServer_PRECISION_NSEC,
+                                nNanoSeconds)) != OS_SUCCESS)
+    {
+        Debug_LOG_ERROR("TimeServer_sleep() failed with %d", err);
+    }
+}
+
+unsigned
+StartKernelTimer(
+    unsigned nHzDelay,
+    TKernelTimerHandler* pHandler,
+    void* pParam,
+    void* pContext)
+{
+    Debug_LOG_WARNING("Not implemented!");
+    return 0;
+}
+
+void
+CancelKernelTimer(unsigned hTimer)
+{
+    Debug_LOG_WARNING("Not implemented!");
+}
+
+/*
+ * Not necessary => used for configuring the usb interrupt which is in our
+ * environment done by camkes
+ */
+void
+ConnectInterrupt(
+    unsigned nIRQ,
+    TInterruptHandler* pHandler,
+    void* pParam)
+{
+    Debug_LOG_WARNING("Not implemented!");
+}
+
+void
+DisconnectInterrupt(unsigned nIRQ)
+{
+    Debug_LOG_WARNING("Not implemented!");
+}
+
+int
+SetPowerStateOn(unsigned nDeviceId)
+{
+    return mailbox_set_power_state_on(&mbox, nDeviceId) ? 1 : 0;
+}
+
+int
+GetMACAddress(unsigned char Buffer[6])
+{
+    return mailbox_get_mac_address(&mbox, Buffer) ? 1 : 0;
+}
+
+unsigned
+GetClockTicks(void)
+{
+    /*
+     * This function overwrites the GetClockTicks() function provided in
+     * timer.cpp in the circle library. The implementation reads from the
+     * System Timer peripheral. This implementation can be extracted to this
+     * environment file but additional modifications would need to be done:
+     *
+     *  - create SystemTimer instance in CAmkES
+     *  - refer to SystemTimer base address defined in CAmkES in bcm2835.h of
+     *    the circle library
+     *  - reference additional header files of the circle library in this
+     *    file
+     *  - read from the CLO register of the System Timer peripheral
+     *
+     * The purpose of this environment file is to create an interface to
+     * TRENTOS functionality for the circle library. If we implement the
+     * GetClockTicks() as described above, we would have dependencies back to
+     * the circle library.
+     *
+     * Fortunately, TRENTOS provides already a TimeServer component that has
+     * an RPC TimeServer_getTime() which will call the SystemTimer anyways.
+     * That way, we can do without defining the System Timer in CAmkES,
+     * remove dependencies to the circle library and leave the circle library
+     * itself in a more clean state.
+     */
+    uint64_t result = 0;
+    // Since the SystemTimer runs at 1MHz, the clock ticks have usec precision.
+    TimeServer_getTime(&timer, TimeServer_PRECISION_USEC, &result);
+    return (unsigned) result;
+}
+
+/* Public functions ----------------------------------------------------------*/
+
+int
+mbox_init(ps_io_ops_t* io_ops)
+{
+    int ret = mailbox_init(io_ops, &mbox);
+    if (ret < 0)
+    {
+        Debug_LOG_ERROR("Failed to initialize mailbox - error code: %d", ret);
+        return ret;
+    }
+    return 0;
+}
+
+}
